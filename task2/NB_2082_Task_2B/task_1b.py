cb2aceab315da0de20dd940df0def2be24dccb9a
Harsh Gandhi <18harshgandhi@gmail.com>
2020-11-28 14:35:27 +0530

    Add files via upload

---
+++ task2/NB_2082_Task_2B/task_1b.py
@@ -0,0 +1,303 @@
+'''
+*****************************************************************************************
+*
+*        		===============================================
+*           		Nirikshak Bot (NB) Theme (eYRC 2020-21)
+*        		===============================================
+*
+*  This script is to implement Task 1B of Nirikshak Bot (NB) Theme (eYRC 2020-21).
+*  
+*  This software is made available on an "AS IS WHERE IS BASIS".
+*  Licensee/end user indemnifies and will keep e-Yantra indemnified from
+*  any and all claim(s) that emanate from the use of the Software or 
+*  breach of the terms of this agreement.
+*  
+*  e-Yantra - An MHRD project under National Mission on Education using ICT (NMEICT)
+*
+*****************************************************************************************
+'''
+
+# Team ID:			[ NB_2082 ]
+# Author List:		[Aditya, Harsh]
+# Filename:			task_1b.py
+# Functions:		applyPerspectiveTransform, detectMaze, writeToCsv
+# 					[ applyPerspectiveTransform, detectMaze, writeToCsv ]
+# Global variables:	
+# 					[  ]
+
+
+####################### IMPORT MODULES #######################
+## You are not allowed to make any changes in this section. ##
+## You have to implement this task with the three available ##
+## modules for this task (numpy, opencv, csv)               ##
+##############################################################
+import numpy as np
+import cv2
+import csv
+##############################################################
+
+
+################# ADD UTILITY FUNCTIONS HERE #################
+## You can define any utility functions for your code.      ##
+## Please add proper comments to ensure that your code is   ##
+## readable and easy to understand.                         ##
+##############################################################
+
+
+
+
+
+
+##############################################################
+
+
+def applyPerspectiveTransform(input_img):
+
+	"""
+	Purpose:
+	---
+	takes a maze test case image as input and applies a Perspective Transfrom on it to isolate the maze
+
+	Input Arguments:
+	---
+	`input_img` :   [ numpy array ]
+		maze image in the form of a numpy array
+	
+	Returns:
+	---
+	`warped_img` :  [ numpy array ]
+		resultant warped maze image after applying Perspective Transform
+	
+	Example call:
+	---
+	warped_img = applyPerspectiveTransform(input_img)
+	"""
+
+	warped_img = None
+
+	##############	ADD YOUR CODE HERE	##############
+	gray = cv2.cvtColor(input_img,cv2.COLOR_BGR2GRAY)
+	gray = np.float32(gray)
+	dst = cv2.cornerHarris(gray,2,3,0.04)
+	dst = cv2.dilate(dst,None)
+	ret, dst = cv2.threshold(dst,0.01*dst.max(),255,0)
+	dst = np.uint8(dst)
+	ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
+	criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
+	corners = cv2.cornerSubPix(gray,np.float32(centroids),(5,5),(-1,-1),criteria)
+	rect = np.zeros((4, 2), dtype = "float32")
+	s = corners.sum(axis = 1)
+	rect[0] = corners[np.argmin(s)]
+	rect[2] = corners[np.argmax(s)]
+	diff = np.diff(corners, axis = 1)
+	rect[1] = corners[np.argmin(diff)]
+	rect[3] = corners[np.argmax(diff)]
+	(tl, tr, br, bl) = rect
+	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+	maxWidth = max(int(widthA), int(widthB))
+	heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+	heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+	maxHeight = max(int(heightA), int(heightB))
+	dst = np.array([[0, 0],[maxWidth - 1, 0],[maxWidth - 1, maxHeight - 1],[0, maxHeight - 1]], dtype = "float32")
+	M = cv2.getPerspectiveTransform(rect, dst)
+	warped_img = cv2.warpPerspective(input_img, M, (maxWidth, maxHeight))
+
+	##################################################
+
+	return warped_img
+
+
+def detectMaze(warped_img):
+
+	"""
+	Purpose:
+	---
+	takes the warped maze image as input and returns the maze encoded in form of a 2D array
+
+	Input Arguments:
+	---
+	`warped_img` :    [ numpy array ]
+		resultant warped maze image after applying Perspective Transform
+	
+	Returns:
+	---
+	`maze_array` :    [ nested list of lists ]
+		encoded maze in the form of a 2D array
+
+	Example call:
+	---
+	maze_array = detectMaze(warped_img)
+	"""
+
+	maze_array = []
+
+	##############	ADD YOUR CODE HERE	##############
+	warped_resize = cv2.resize(warped_img, (350,350))
+	cell = int(warped_resize.shape[0]/10)
+	gray = cv2.cvtColor(warped_resize, cv2.COLOR_BGR2GRAY)
+	(blackAndWhiteImage) = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 199, 5)
+	for i in range(10):
+		d_list = []
+		for j in range(10):
+			sum_up, sum_down, sum_left, sum_right, sum  = 0, 0 ,0 , 0, 0
+			cropped = blackAndWhiteImage[i*(cell):(i+1)*(cell), j*(cell):(j+1)*(cell)]
+			for k in range(cell):
+				sum_left = sum_left + cropped[k,0]
+				sum_right = sum_right + cropped[k,cell-1]
+				sum_up = sum_up + cropped[0,k]
+				sum_down = sum_down + cropped[cell-1,k]
+			if sum_left/cell<127:
+				sum = sum + 1
+			if sum_right/cell<127:
+				sum = sum + 4
+			if sum_up/cell<127:
+				sum = sum + 2
+			if sum_down/cell<127:
+				sum = sum + 8
+			d_list.append(sum)
+		maze_array.append(d_list)
+
+	##################################################
+
+	return maze_array
+
+
+# NOTE:	YOU ARE NOT ALLOWED TO MAKE ANY CHANGE TO THIS FUNCTION
+def writeToCsv(csv_file_path, maze_array):
+
+	"""
+	Purpose:
+	---
+	takes the encoded maze array and csv file name as input and writes the encoded maze array to the csv file
+
+	Input Arguments:
+	---
+	`csv_file_path` :	[ str ]
+		file path with name for csv file to write
+	
+	`maze_array` :		[ nested list of lists ]
+		encoded maze in the form of a 2D array
+	
+	Example call:
+	---
+	warped_img = writeToCsv('test_cases/maze00.csv', maze_array)
+	"""
+
+	with open(csv_file_path, 'w', newline='') as file:
+		writer = csv.writer(file)
+		writer.writerows(maze_array)
+
+
+# NOTE:	YOU ARE NOT ALLOWED TO MAKE ANY CHANGE TO THIS FUNCTION
+# 
+# Function Name:    main
+#        Inputs:    None
+#       Outputs:    None
+#       Purpose:    This part of the code is only for testing your solution. The function first takes 'maze00.jpg'
+# 					as input, applies Perspective Transform by calling applyPerspectiveTransform function,
+# 					encodes the maze input in form of 2D array by calling detectMaze function and writes this data to csv file
+# 					by calling writeToCsv function, it then asks the user whether to repeat the same on all maze images
+# 					present in 'test_cases' folder or not. Write your solution ONLY in the space provided in the above
+# 					applyPerspectiveTransform and detectMaze functions.
+
+if __name__ == "__main__":
+
+	# path directory of images in 'test_cases' folder
+	img_dir_path = 'test_cases/'
+
+	# path to 'maze00.jpg' image file
+	file_num = 0
+	img_file_path = img_dir_path + 'maze0' + str(file_num) + '.jpg'
+
+	print('\n============================================')
+	print('\nFor maze0' + str(file_num) + '.jpg')
+
+	# path for 'maze00.csv' output file
+	csv_file_path = img_dir_path + 'maze0' + str(file_num) + '.csv'
+	
+	# read the 'maze00.jpg' image file
+	input_img = cv2.imread(img_file_path)
+
+	# get the resultant warped maze image after applying Perspective Transform
+	warped_img = applyPerspectiveTransform(input_img)
+
+	if type(warped_img) is np.ndarray:
+
+		# get the encoded maze in the form of a 2D array
+		maze_array = detectMaze(warped_img)
+
+		if (type(maze_array) is list) and (len(maze_array) == 10):
+
+			print('\nEncoded Maze Array = %s' % (maze_array))
+			print('\n============================================')
+			
+			# writes the encoded maze array to the csv file
+			writeToCsv(csv_file_path, maze_array)
+
+			cv2.imshow('warped_img_0' + str(file_num), warped_img)
+			cv2.waitKey(0)
+			cv2.destroyAllWindows()
+		
+		else:
+
+			print('\n[ERROR] maze_array returned by detectMaze function is not complete. Check the function in code.\n')
+			exit()
+	
+	else:
+
+		print('\n[ERROR] applyPerspectiveTransform function is not returning the warped maze image in expected format! Check the function in code.\n')
+		exit()
+	
+	choice = input('\nDo you want to run your script on all maze images ? => "y" or "n": ')
+
+	if choice == 'y':
+
+		for file_num in range(1, 10):
+			
+			# path to image file
+			img_file_path = img_dir_path + 'maze0' + str(file_num) + '.jpg'
+
+			print('\n============================================')
+			print('\nFor maze0' + str(file_num) + '.jpg')
+
+			# path for csv output file
+			csv_file_path = img_dir_path + 'maze0' + str(file_num) + '.csv'
+			
+			# read the image file
+			input_img = cv2.imread(img_file_path)
+
+			# get the resultant warped maze image after applying Perspective Transform
+			warped_img = applyPerspectiveTransform(input_img)
+
+			if type(warped_img) is np.ndarray:
+
+				# get the encoded maze in the form of a 2D array
+				maze_array = detectMaze(warped_img)
+
+				if (type(maze_array) is list) and (len(maze_array) == 10):
+
+					print('\nEncoded Maze Array = %s' % (maze_array))
+					print('\n============================================')
+					
+					# writes the encoded maze array to the csv file
+					writeToCsv(csv_file_path, maze_array)
+
+					cv2.imshow('warped_img_0' + str(file_num), warped_img)
+					cv2.waitKey(0)
+					cv2.destroyAllWindows()
+				
+				else:
+
+					print('\n[ERROR] maze_array returned by detectMaze function is not complete. Check the function in code.\n')
+					exit()
+			
+			else:
+
+				print('\n[ERROR] applyPerspectiveTransform function is not returning the warped maze image in expected format! Check the function in code.\n')
+				exit()
+
+	else:
+
+		print('')
+
